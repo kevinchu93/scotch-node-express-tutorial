ca4cd840fcd908391ea51490e5452da88e6e76b7
Kevin Chu <kevinxchu@gmail.com>
2018-01-03 17:37:40 +0900

    finished tutorial

--- server.js
+++ server.js
@@ -3,6 +3,12 @@
 // BASE SETUP
 // ===============================================================
 //
+//
+var mongoose  = require('mongoose');
+mongoose.connect('mongodb://test:test@ds239557.mlab.com:39557/scotchdb');  // connect to
+
+var Bear = require('./app/models/bear');
+
 // call the packages we need
 var express     = require('express');       // call express
 var app         = express();                // define our app using express
@@ -19,13 +25,99 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
 // more routes for our API will happen here
-//
+
+// on routes that end in /bears
+// -----------------------------------------------------------
+router.route('/bears')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+
+        var bear = new Bear();  // create a new instance of the Bear model
+        bear.name = req.body.name;  // set the bears name (comes form the request)
+        bear.age = req.body.age;    // set the bear's age
+        bear.rank = count;
+        count = count + 1;
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Bear created!' });
+        });
+
+    })
+
+    // get all the bears (accessed at GET http://localhost:8080/api/bears)
+    .get(function(req, res) {
+        Bear.find(function(err, bears) {
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+        });
+    });
+
+// on routes that end in /bears/:bear_id
+// ----------------------------------------------------
+var count = 1;
+router.route('/bears/:bear_id')
+
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+    })
+
+    // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
+    .put(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            
+            if (err)
+                res.send(err);
+
+            bear.name = req.body.name;  // update the bears info
+
+            // save the bear
+            bear.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated!' });
+            });
+
+        });
+    })
+
+    // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
+    .delete(function(req, res) {
+        Bear.remove({
+            _id: req.params.bear_id
+        }, function(err, bear) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted!' });
+        });
+    });
+
 // REGISTER OUR ROUTES ---------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
